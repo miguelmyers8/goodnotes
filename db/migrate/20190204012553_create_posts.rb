5823b92cf03c0e29b80b5ce2a9fe55622c1c0713
miguel myers <myers.m007@gmail.com>
2019-02-06 21:25:49 -0500

    devise users

--- db/migrate/20190204012553_create_posts.rb
+++ db/migrate/20190204012553_create_posts.rb
@@ -1,9 +1,10 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :name
+      t.string :title
+      t.string :subject
+      t.text :description
       t.boolean :f_type
-      t.text :comment
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_04_034627) do
+ActiveRecord::Schema.define(version: 2019_02_07_021641) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -34,11 +34,27 @@ ActiveRecord::Schema.define(version: 2019_02_04_034627) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "name"
+    t.string "title"
+    t.string "subject"
+    t.text "description"
     t.boolean "f_type"
-    t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "school"
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
